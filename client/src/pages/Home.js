781d8f65894ac6571332f39987d3ebf0dd8a1d9e
Jesse Emerson <jesseemerson7@gmail.com>
2023-06-06 11:37:19 -0400

    blank page ready for dev

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 
-import ProfileList from '../components/ProfileList';
-
 import { QUERY_PROFILES } from '../utils/queries';
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_PROFILES);
-  const profiles = data?.profiles || [];
+
 
   return (
     <main>
-      
+     
     </main>
   );
 };
