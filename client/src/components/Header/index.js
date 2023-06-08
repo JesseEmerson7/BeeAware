6ed953dc6843844ac27b33c0024cffb349f46b7b
AlexC3105 <126286847+AlexC3105@users.noreply.github.com>
2023-06-08 10:03:10 -0400

    updated css on main

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,15 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
-import logo from ".././../../src/assets/logo/BeeAware3.png";
-
-// import Auth from "../../utils/auth";
+import logo from ".././../../src/assets/logo/BeeAware1.png";
 
 const Header = () => {
-  // const logout = (event) => {
-  //   event.preventDefault();
-  //   Auth.logout();
-  // };
   return (
     <>
       <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
@@ -18,7 +12,7 @@ const Header = () => {
             <img
               id="logo-img"
               src={logo}
-              className="h-8 mr-3"
+              className="h-8 mr-3 logo-flip"
               alt="BeeAware Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
@@ -41,9 +35,9 @@ const Header = () => {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </button>
@@ -51,7 +45,7 @@ const Header = () => {
             className="hidden w-full md:block md:w-auto"
             id="navbar-dropdown"
           >
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 zigzag-menu">
               <li>
                 <Link
                   to="/Info"
@@ -61,7 +55,6 @@ const Header = () => {
                   Info
                 </Link>
               </li>
-
               <li>
                 <Link
                   to="/blog"
@@ -70,9 +63,7 @@ const Header = () => {
                   Blog
                 </Link>
               </li>
-
               <li>
-                {/* Placeholder url in to path */}
                 <Link
                   to="/profiles/123345"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -95,13 +86,12 @@ const Header = () => {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                 </button>
-                {/* <!-- Dropdown menu --> */}
                 <div
                   id="dropdownNavbar"
                   className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
@@ -140,7 +130,6 @@ const Header = () => {
                       to="/sign"
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
                     >
-                      {/* this needs to change based on context of user */}
                       Sign out/Sign in
                     </Link>
                   </div>
