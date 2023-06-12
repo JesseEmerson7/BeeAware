ca8f5b7bbfc80abd21f6459c175283c1ebbc7e84
Jesse Emerson <jesseemerson7@gmail.com>
2023-06-12 11:40:02 -0400

    added date formatting to post

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_SINGLE_POST } from "../utils/queries";
 import './SinglePost.css'
+// import { create } from "../../../server/models/Post";
 
 const SinglePost = () => {
   const {id} = useParams();
@@ -26,10 +27,19 @@ const SinglePost = () => {
   }
 
   const {title, body, createdAt} = postData;
+
+  const dateNumber = parseInt(createdAt)
+  const date = new Date(dateNumber);
+  const formattedDate = date.toLocaleString();
+console.log(dateNumber);
+console.log(formattedDate);
+
+
+
   return (
     <div id="post-card" className="text-center flex flex-col  h-screen ">
     <div className="my-5 md:mt-20  "> <h1 className="font-semibold text-xl">{title}</h1>
-      <h1 className="text-sm">Posted on: {createdAt}</h1>
+      <h1 className="text-sm">{formattedDate}</h1>
       </div>
     <p className="text-lg w-5/6 mx-auto">{body}</p>
      
