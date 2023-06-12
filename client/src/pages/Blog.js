50e57b1d4ee1a9485f318ac42894406202e5fde2
Jesse Emerson <jesseemerson7@gmail.com>
2023-06-12 09:55:42 -0400

    changed small thing for fix

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -11,7 +11,7 @@ const Blog = () => {
   }
 
   console.log(data.allPosts);
-  const mapList = data.allPosts;
+  const mapList = data?.allPosts;
 // map over post list and create blog list
   const mapPostList = mapList.map((post) => {
     return (
