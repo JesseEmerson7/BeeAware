1364e2e88e1422e41b8200d7f5d1a082e1301803
Jesse Emerson <jesseemerson7@gmail.com>
2023-07-06 22:52:16 -0400

    comments on edit profile page

--- client/src/pages/UpdateProfile.js
+++ client/src/pages/UpdateProfile.js
@@ -6,14 +6,24 @@ import auth from "../utils/auth";
 const UpdateProfile = () => {
   const userInfo = auth.getProfile();
 
+  //query to grab user profile info from the logged in profile.
   const { loading, data } = useQuery(QUERY_USER_PROFILE, {
     variables: { userId: userInfo.data._id },
   });
+
+  //state for the form from user data
   const [userData, ChangeUserData] = useState(data?.user);
   console.log(userData);
 
   useEffect(() => {});
 
+  //handel form change to update the state of the user data
+  const handleFormChange = (event) => {};
+
+  // on submit the user data will use mutation to change the database and update the profile page.
+  const handleFormSubmit = (event) => {};
+
+  //if user data has not been fetched a loading indicator will be displayed
   if (loading) {
     return (
       <>
@@ -22,25 +32,28 @@ const UpdateProfile = () => {
     );
   }
 
+  // if loading is done then the form is displayed with the user info
   return (
     <>
       <div>
         Welcome {userData.username}, Here you can edit your profile information.
       </div>
-      <div className="mb-6">
-        <label
-          for="base-input"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Change Profile Bio
-        </label>
-        <input
-          type="text"
-          name="bio"
-          value={userData.bio}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        />
-      </div>
+      <form onChange={handleFormChange} onSubmit={handleFormSubmit}>
+        <div className="mb-6">
+          <label
+            for="base-input"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Change Profile Bio
+          </label>
+          <input
+            type="text"
+            name="bio"
+            value={userData.bio}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+      </form>
     </>
   );
 };
