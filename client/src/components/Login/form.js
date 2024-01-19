5decb5e4a7e81d5631b26e9569a90de1e3d85e92
Jesse Emerson <jesseemerson7@gmail.com>
2024-01-19 17:25:18 -0500

    database query complete for user login

--- client/src/components/Login/form.js
+++ client/src/components/Login/form.js
@@ -3,10 +3,10 @@ import EyeSvg from "./eyeSvg";
 import "./form.css";
 import logo from "../../assets/logo/BeeAware3.png";
 // ---- for future use
-// import { useMutation } from "@apollo/client";
-// import { LOGIN_USER } from "../../utils/mutations";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../../utils/mutations";
 
-// import Auth from "../../utils/auth";
+import Auth from "../../utils/auth";
 // import { Link } from "react-router-dom";
 // ----
 
@@ -22,13 +22,14 @@ const Form = ({ setPage }) => {
   //----creating states---
   const [passwordState, setPasswordState] = useState("password");
   const [formInputs, changeFormInputs] = useState({
-    userId: "",
+    email: "",
     password: "",
   });
   const [userInputColor, changeUserColor] = useState(true);
   const [passInputColor, changePassColor] = useState(true);
   const [submitErr, changeErr] = useState(false);
   const [loginBtnValue, changeLoginBtn] = useState("Log-in");
+  const [login, { error }] = useMutation(LOGIN_USER);
   //----component functions----
   //toggle to view password
   const handleEyeClick = () => {
@@ -40,7 +41,7 @@ const Form = ({ setPage }) => {
   const handleFormChange = (e) => {
     const { name, value } = e.target;
     //changing color back to green if not empty
-    if (name === "userId") {
+    if (name === "email") {
       changeUserColor(true);
     } else {
       changePassColor(true);
@@ -52,7 +53,7 @@ const Form = ({ setPage }) => {
     });
   };
   //on submit handler
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     /*Here I would send a POST request to the server side. I would then make a database query with that information (SELECT * FROM users
 WHERE username = userId AND password = password;) The password would be unHashed and compared. response would be sent back to the front end. If no match error message would be displayed above login button
 A token would be added to the user as well when logged in.
@@ -61,21 +62,31 @@ A token would be added to the user as well when logged in.
     //temp input checker until server side is done.
     changeErr(false);
     changeLoginBtn("Submitting...");
-    if (formInputs.userId === "admin" && formInputs.password === "admin") {
-      changeLoginBtn("Log-in");
-      changeFormInputs({
-        userId: "",
-        password: "",
+    try {
+      const { data } = await login({
+        variables: { ...formInputs },
       });
-      setPage(false);
-      // window.location = "https://github.com/JesseEmerson7";
-    } else if (!formInputs.userId) {
-      firstInputRef.current.focus();
-    } else if (!formInputs.password) {
-      secondInputRef.current.focus();
-    } else {
-      changeErr(true);
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.log(e);
+      //ToDO! handle wrong input here  --------------------------------------
     }
+
+    // if (formInputs.userId === "admin" && formInputs.password === "admin") {
+    //   changeLoginBtn("Log-in");
+    //   changeFormInputs({
+    //     userId: "",
+    //     password: "",
+    //   });
+    //   setPage(false);
+    //   // window.location = "https://github.com/JesseEmerson7";
+    // } else if (!formInputs.userId) {
+    //   firstInputRef.current.focus();
+    // } else if (!formInputs.password) {
+    //   secondInputRef.current.focus();
+    // } else {
+    //   changeErr(true);
+    // }
     changeLoginBtn("Log-in");
   };
   //onBlur of inputs
@@ -95,8 +106,15 @@ A token would be added to the user as well when logged in.
       <div className="w-full lg:w-5/12 xl:w-4/12 md:w-8/12 md:shadow-2xl md:p-16 md:my-10  md:mx-auto md:rounded-sm px-4 pt-2 bg-amber-100 min-h-screen border-solid border-amber-500 border">
         {/* title */}
         <div className="mb-4 text-center">
-          <span className=" text-4xl " id="titleLogin">BeeAware Login</span>
-          <img src={logo} alt="Logo" width={"200px"} className=" rounded-full mx-auto mt-3"/>
+          <span className=" text-4xl " id="titleLogin">
+            BeeAware Login
+          </span>
+          <img
+            src={logo}
+            alt="Logo"
+            width={"200px"}
+            className=" rounded-full mx-auto mt-3"
+          />
         </div>
         {/* form user / password */}
         <form onSubmit={(e) => handleSubmit(e)}>
@@ -115,7 +133,7 @@ A token would be added to the user as well when logged in.
             <input
               onChange={(e) => handleFormChange(e)}
               onBlur={handleEmptyInput}
-              name="userId"
+              name="email"
               autofocus
               type="text"
               required
@@ -176,7 +194,6 @@ A token would be added to the user as well when logged in.
           </div>
 
           {/* info */}
-          
         </form>
       </div>
     </section>
