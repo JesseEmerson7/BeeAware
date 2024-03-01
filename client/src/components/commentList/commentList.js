e93153401e6e2cc9cb52e1474ebf4fd004933f51
Jesse Emerson <jesseemerson7@gmail.com>
2024-03-01 18:23:35 -0500

    adding comment post button

--- client/src/components/commentList/commentList.js
+++ client/src/components/commentList/commentList.js
@@ -1,15 +1,25 @@
 import "./commentList.css";
 import { formatDistanceToNow } from "date-fns";
+import { useState } from "react";
 
-export default function commentList({ comments }) {
+export default function CommentList({ comments }) {
+  //states
+  const [CommentPop, changeCommentPop] = useState(false);
+  const [commentText, updateCommentText] = useState("")
+
   //handle comment like and adding or subtracting value to DB
   const handleLike = () => {
     //toDO: create mutation to add likes to DB and add 1 to the like value locally
   };
+  //comment post button or cancel
+  const handleCommentButton = (e) => {
+    e.preventDefault();
+    //toDO: MUTATION NEEDED
+  };
 
   let commentsListArr = comments.map((comment) => {
     return (
-      <div className="commentDiv bg-white rounded-sm m-3">
+      <div key={comment._id} className="commentDiv bg-white rounded-sm m-3">
         <div className="flex ml-5">
           <h3 className=" text-lg">{comment.author}</h3>
           <span className="ml-3 mt-1 text-xs text-slate-400">
@@ -45,13 +55,31 @@ export default function commentList({ comments }) {
           {comments.length > 0 ? `Comments (${comments.length})` : <></>}
         </h3>
         {/* add a comment */}
-        <div>
+        <form onSubmit={ handleCommentButton}>
+          {/* comment input box */}
           <input
             type="text"
             placeholder="Add a comment..."
-            className=" w-5/6 m-3 rounded-lg border-none"
+            className=" w-5/6 m-3 mb-0 rounded-lg border-none"
+            onFocus={() => changeCommentPop(true)}
+            onChange={(e)=> updateCommentText( e.target.value)}
           />
-        </div>
+          <div
+            className={` space-x-3 text-right w-5/6 mt-2  ${
+              CommentPop ? "" : "hidden"
+            } `}
+          >
+            <button
+              className=" rounded-xl p-1 hover:bg-amber-500 duration-100"
+              onClick={() => changeCommentPop(false)}
+            >
+              Cancel
+            </button>
+            <button type="submit" className=" bg-amber-400 rounded-xl p-1 hover:bg-amber-500 duration-300 " >
+              Comment
+            </button>
+          </div>
+        </form>
         <div>{comments.length > 0 ? commentsListArr : <></>}</div>
       </div>
     );
